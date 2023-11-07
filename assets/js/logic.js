4561a3a45490cd000a1cbf0c15d8b1567dc12605
Tom Paling <137108048+ph8tfunk@users.noreply.github.com>
2023-11-07 22:25:01 +0000

    added question to quiz object. added gif of website

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,14 +1,40 @@
 var quiz = [{
 
-    question: 1,
-    choices: ["a","b","c","d"],
-    answer: "b"
+    question: "Inside which HTML element do we put the JavaScript?",
+    choices: ["<p>","<script>","<src>","<h1>"],
+    answer: "<script>"
 },
 {
-    question: 2,
-    choices: ["a","b","c","d"],
-    answer: "a"
-}]
+    question: "What data types has true/false values",
+    choices: ["Boolean","Strings","Numbers","NaN"],
+    answer: "Boolean"
+},
+{
+    question: "Commonly used data types DO NOT include:",
+    choices: ["Strings", "Booleans", "Alerts", "Numbers"],
+    answer: "Alerts"
+},
+{
+    question: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses"
+},
+{
+    question: "A very useful tool for used during development and debugging for printing content to the debugger is:",
+    choices: ["Javascript", "terminal / bash", "for loops", "console log"],
+    answer: "console log"
+},
+{
+    question: "Arrays in Javascript can be used to store ____.",
+    choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+    answer: "all of the above"
+},
+{
+    question: "String values must be enclosed within ____ when being assigned to variables.",
+    choices:  ["commas", "curly brackets", "quotes", "parenthesis"],
+    answer: "quotes"
+}
+]
 
 
 var questions = document.getElementById("questions");
